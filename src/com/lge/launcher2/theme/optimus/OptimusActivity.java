96bcf516b6a62348f205adfdc7e1c17e024325cc
Solitarily <chuangweida@gmail.com>
2014-07-15 22:56:27 +0800

    First upload

---
+++ src/com/lge/launcher2/theme/optimus/OptimusActivity.java
@@ -0,0 +1,13 @@
+package com.lge.launcher2.theme.optimus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OptimusActivity extends Activity
+{
+  public void onCreate(Bundle paramBundle)
+  {
+    super.onCreate(paramBundle);
+    setContentView(2130903040);
+  }
+}
